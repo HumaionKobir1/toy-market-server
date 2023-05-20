ae0d56749a83d5959122df99076fa23ebc09e2c8
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-20 20:02:10 +0600

    get all toy  data from server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,16 +28,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('toyMarket').collection('toyProduct');
+
+    app.get('/allToy', async(req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/toy', async(req, res) => {
+      const newToy = req.body;
+      console.log(newToy);
+      const result = await toyCollection.insertOne(newToy);
+      res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
+
   }
 }
 run().catch(console.dir);
-
 
 
 
